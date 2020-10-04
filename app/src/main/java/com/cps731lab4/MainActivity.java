98d0abe3b2e3cc1051ad76c3b2d3ebbfdeab0a19
lauramcguire <laura.g.mcguire@ryerson.ca>
2020-10-03 23:36:20 -0400

    Created adapter constructor and new instance in main

--- app/src/main/java/com/cps731lab4/MainActivity.java
+++ app/src/main/java/com/cps731lab4/MainActivity.java
@@ -1,6 +1,7 @@
 package com.cps731lab4;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
@@ -9,12 +10,10 @@ public class MainActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
 
-    String[] recipeStr;
-    String[] descriptionStr;
-    String[] ingredientsStr;
+    String[] recipeStr, descriptionStr, ingredientsStr;
     int[] images = {R.drawable.img1, R.drawable.img2, R.drawable.img3, R.drawable.img4, R.drawable.img5,
-            R.drawable.img6, R.drawable.img7, R.drawable.img8, R.drawable.img9, R.drawable.img10,
-            R.drawable.img11, R.drawable.img12, R.drawable.img13};
+                    R.drawable.img6, R.drawable.img7, R.drawable.img8, R.drawable.img9, R.drawable.img10,
+                    R.drawable.img11, R.drawable.img12, R.drawable.img13};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +25,9 @@ public class MainActivity extends AppCompatActivity {
         recipeStr = getResources().getStringArray(R.array.recipes);
         descriptionStr = getResources().getStringArray(R.array.description);
         ingredientsStr = getResources().getStringArray(R.array.ingredients);
+
+        RecyclerViewAdapter myAdapter = new RecyclerViewAdapter(this, recipeStr, descriptionStr, ingredientsStr, images);
+        recyclerView.setAdapter(myAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
--- app/src/main/java/com/cps731lab4/RecyclerViewAdapter.java
+++ app/src/main/java/com/cps731lab4/RecyclerViewAdapter.java
@@ -1,5 +1,6 @@
 package com.cps731lab4;
 
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -7,6 +8,17 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder> {
+    Context context;
+    String[] s1, s2, s3;
+    int[] images;
+
+    public RecyclerViewAdapter(Context cont, String[] str1, String[] str2, String[] str3, int[] img){
+        context = cont;
+        s1 = str1;
+        s2 = str2;
+        s3 = str3;
+        images = img;
+    }
 
     @NonNull
     @Override
