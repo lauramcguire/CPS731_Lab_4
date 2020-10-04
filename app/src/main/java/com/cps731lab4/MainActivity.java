caacbe12cf62d69b46a99e5d3b509f5e956faa16
lauramcguire <laura.g.mcguire@ryerson.ca>
2020-10-03 22:08:43 -0400

    Initialized Strings and drawables

--- app/src/main/java/com/cps731lab4/MainActivity.java
+++ app/src/main/java/com/cps731lab4/MainActivity.java
@@ -6,6 +6,13 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    String[] recipeStr;
+    String[] descriptionStr;
+    String[] ingredientsStr;
+    int[] images = {R.drawable.img1, R.drawable.img2, R.drawable.img3, R.drawable.img4, R.drawable.img5,
+            R.drawable.img6, R.drawable.img7, R.drawable.img8, R.drawable.img9, R.drawable.img10,
+            R.drawable.img11, R.drawable.img12, R.drawable.img13};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
