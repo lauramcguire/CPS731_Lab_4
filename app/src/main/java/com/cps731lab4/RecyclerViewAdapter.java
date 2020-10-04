3b2095ef4ace7c300b7ac028108fcd9e7c7697ed
lauramcguire <laura.g.mcguire@ryerson.ca>
2020-10-03 23:57:23 -0400

    Implemented RecyclerViewAdapter methods

--- app/src/main/java/com/cps731lab4/RecyclerViewAdapter.java
+++ app/src/main/java/com/cps731lab4/RecyclerViewAdapter.java
@@ -1,8 +1,12 @@
 package com.cps731lab4;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -23,12 +27,15 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.my_row, parent, false);
+        return new MyViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-
+        holder.title.setText(s1[position]);
+        holder.description.setText(s2[position]);
     }
 
     @Override
@@ -37,9 +44,14 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView title, description;
+        LinearLayout mainLayout;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+            title = itemView.findViewById(R.id.recipeTitle);
+            description = itemView.findViewById(R.id.recipeDescription);
+            mainLayout = itemView.findViewById(R.id.mainLayout);
         }
     }
 }
